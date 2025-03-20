2f7a84491f58a43d69a167ced5408dc771c62849
Israel Fernandez <israelfernandez96@gmail.com>
2025-03-19 22:19:30 -0700

    Time and Embeddings model. Fixed tabbing for main Application

--- demo/src/main/java/com/example/demo/model/Time.java
+++ demo/src/main/java/com/example/demo/model/Time.java
@@ -9,9 +9,25 @@ public class Time {
         return currentDate.toString(); // 2023-11-03
     }
 
+    public static String getTime() {
+        LocalTime currentTime = LocalTime.now();
+        return currentTime.toString(); // 10:15:30.123456789
+    }
+
+    public static String getDateTime() {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        return currentDateTime.toString(); // 2023-11-03T10:15:30.123456789
+    }
+
     public static String getCurrentBeginningOfWeek() {
         LocalDate today = LocalDate.now();
         LocalDate startOfWeek = today.with(TemporalAdjusters.previousOrSame(DayOfWeek.SUNDAY));
         return startOfWeek.toString(); // 2023-10-15
+    }
+
+    public static String getCurrentEndOfWeek() {
+        LocalDate today = LocalDate.now();
+        LocalDate endOfWeek = today.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+        return endOfWeek.toString(); // 2023-10-21
     }
 }
